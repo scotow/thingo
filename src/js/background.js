b7e5124d8501d87b4c732051e824dd3e82edadae
Lopez Benjamin <benjamin.lopez@etudiant.univ-lille1.fr>
2018-03-07 10:27:55 +0100

    Move popup extractor to background script

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,86 @@
-async function testbg() {
-    setTimeout(function () {
-        const blob = new Blob([JSON.stringify([1, 2, 3], null, '\t')], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
+async function currentTab() {
+    const options = {
+        active: true,
+        currentWindow: true
+    };
 
-        await browser.downloads.download({
-            url: url,
-            filename: 'data.json',
-        });
-    }, 3000);
+    // Get current active tab.
+    const tabs = await browser.tabs.query(options);
+
+    // Reject promise if there is no or more than one tab.
+    if(tabs.length !== 1) throw new Error('Number of active tab incorrect.');
+
+    return tabs[0];
+}
+
+async function donwloadData(data) {
+    const blob = new Blob([JSON.stringify(data, null, '\t')], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    await browser.downloads.download({
+        url: url,
+        filename: 'data.json'
+    });
+}
+
+function wait(delay) {
+    return new Promise(resolve => {
+        setTimeout(resolve, delay);
+    });
+}
+
+async function extractData(tree) {
+    const tab = await currentTab();
+
+    return await browser.tabs.sendMessage(tab.id, {
+        action: 'extract',
+        tree: tree
+    });
+}
+
+async function nextPage(data) {
+    const tab = await currentTab();
+
+    return await browser.tabs.sendMessage(tab.id, {
+        action: 'next',
+        next: data
+    });
+}
+
+function singlePage(template) {
+    extractData(template.tree)
+    .then(data => donwloadData(data))
+    .catch(error => console.error(error));
+}
+
+function allPages(template) {
+    let data = [];
+
+    new Promise((resolve, reject) => {
+        function extractPage() {
+            wait(1000)
+            .then(() => extractData(template.tree))
+            .then(chunk => data.push(chunk))
+            .then(() => nextPage(template.next))
+            .then(success => {
+                if(!success) {
+                    chrome.webRequest.onCompleted.removeListener(extractPage);
+                    resolve(data);
+                }
+            })
+            .catch(error => {
+                chrome.webRequest.onCompleted.removeListener(extractPage);
+                reject(error);
+            });
+        }
+
+        chrome.webRequest.onCompleted.addListener(extractPage, { urls: [template.next.request + '*'] });
+        extractPage();
+    })
+    .then(data => {
+        return donwloadData(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,7 +1,3 @@
-/**
- * Get the current active tab open in the browser.
- * @return {Promise} A promise wrapping a tab object or an error.
- */
 async function currentTab() {
     const options = {
         active: true,
@@ -15,24 +11,6 @@ async function currentTab() {
     if(tabs.length !== 1) throw new Error('Number of active tab incorrect.');
 
     return tabs[0];
-}
-
-async function extractData(tree) {
-    const tab = await currentTab();
-
-    return await browser.tabs.sendMessage(tab.id, {
-        action: 'extract',
-        tree: tree
-    });
-}
-
-async function nextPage(data) {
-    const tab = await currentTab();
-
-    return await browser.tabs.sendMessage(tab.id, {
-        action: 'next',
-        next: data
-    });
 }
 
 function fetchTemplate(domain) {
@@ -63,59 +41,18 @@ async function loadTemplate() {
     return JSON.parse(await fetchTemplate(domain));
 }
 
-async function donwloadData(data) {
-    const blob = new Blob([JSON.stringify(data, null, '\t')], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-
-    await browser.downloads.download({
-        url: url,
-        filename: 'data.json',
-        saveAs: false
-    });
-}
-
-function wait(delay) {
-    return new Promise(resolve => {
-        setTimeout(resolve, delay);
-    });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('parse').addEventListener('click', () => {
-        let data = [];
-
         loadTemplate()
-        .then(template => {
-            return new Promise((resolve, reject) => {
-                function extractPage() {
-                    wait(1000)
-                    .then(() => extractData(template.tree))
-                    .then(chunk => data.push(chunk))
-                    .then(() => nextPage(template.next))
-                    .then(success => {
-                        if(!success) resolve(data);
-                    })
-                    .catch(error => reject(error));
-                }
-
-                chrome.webRequest.onCompleted.addListener(extractPage, { urls: ["http://www.bm-lille.fr/Default/Portal/Recherche/Search.svc/Search*"] });
-                extractPage();
-            });
-        })
-        .then(data => donwloadData(data))
-        .catch(error => {
-            console.error(error);
-        });
+        .then(template => chrome.extension.getBackgroundPage().allPages(template))
+        .catch(error => console.error(error));
     });
 
     document.getElementById('single').addEventListener('click', () => {
         loadTemplate()
-        .then(template => extractData(template.tree))
-        .then(data => donwloadData(data))
+        .then(template => chrome.extension.getBackgroundPage().singlePage(template))
         .catch(error => console.error(error));
     });
 
-    document.getElementById('load').addEventListener('click', () => {
-        console.log(chrome.extension.getBackgroundPage().testbg());
-    });
+    // document.getElementById('load').addEventListener('click', () => {});
 });
